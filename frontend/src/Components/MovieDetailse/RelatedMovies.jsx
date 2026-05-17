e445b28c8d9b0d20a8e3eec2a462c302b1acfcf2
Harshal <harshalmhaske681@gmail.com>
2026-05-17 20:25:09 +0530

    You Might Also Like Fix

--- frontend/src/Components/MovieDetailse/RelatedMovies.jsx
+++ frontend/src/Components/MovieDetailse/RelatedMovies.jsx
@@ -8,7 +8,7 @@ const RelatedMovies = ({ relatedMovies }) => {
       <h2 className="mb-10 text-3xl font-bold text-white">You Might Also Like</h2>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {relatedMovies.map((relatedMovie) => (
-          <FeatureCard key={relatedMovie.id} show={relatedMovie} />
+          <FeatureCard key={relatedMovie._id || relatedMovie.tmdbId || relatedMovie.id} movie={relatedMovie} />
         ))}
       </div>
     </div>
